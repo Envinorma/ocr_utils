584a1edd469e83d01d7a7e587582a5e931b535ab
remidbs <remi.delbouys@laposte.net>
2021-03-23 15:09:57 +0100

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     url="https://github.com/envinorma/ocr_utils",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.3",
+    version="0.0.4",
     zip_safe=False,
 )
