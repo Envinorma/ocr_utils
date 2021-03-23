bea807f723cd36666bbc181cda4a7b360c88912a
Remi Delbouys <remidbs@users.noreply.github.com>
2021-03-23 11:44:39 +0100

    Correct CI build_main (#2)
    * corrected matrix for build (removed macos/windows)
    
    * Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -87,6 +87,6 @@ setup(
     url="https://github.com/envinorma/ocr_utils",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
